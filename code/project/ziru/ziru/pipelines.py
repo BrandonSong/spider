556ae453f4bcbce6dccb947382e0bed3fd50c52c
BrandonSong <lqisong@163.com>
2018-12-18 17:16:16 +0800

    get price

--- code/project/ziru/ziru/pipelines.py
+++ code/project/ziru/ziru/pipelines.py
@@ -11,3 +11,6 @@ class ZiruPipeline(object):
         """ 对数据进行清洗 """
         item["room_addr"] = item["room_addr"].replace("\n", "")
         item["room_area"] = item["room_area"].replace(" ", "").replace("\n", "")
+        item["room_traffic"] = [i.replace(" ","").replace("\n", "") for i in item["room_traffic"]]
+        item["room_traffic"] = [i for i in item["room_traffic"] if len(i) > 0]
+        print(item)
--- code/project/ziru/ziru/spiders/zr.py
+++ code/project/ziru/ziru/spiders/zr.py
@@ -1,8 +1,12 @@
 # -*- coding: utf-8 -*-
 import scrapy
+import re
+import json
 from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ziru.items import ZiruItem
+from copy import deepcopy
+import pytesseract
 
 
 
@@ -30,9 +34,27 @@ class ZrSpider(CrawlSpider):
        item["room_orientations"] = response.xpath("//ul[@class='detail_room']/li[2]/text()").extract_first().strip()
        item["room_type"] = response.xpath("//ul[@class='detail_room']/li[3]/text()").extract_first().strip()
        item["room_floor"] = response.xpath("//ul[@class='detail_room']/li[4]/text()").extract_first().strip()
-       item["room_traffic"] = response.xpath("//ul[@class='detail_room']/li[5]/span[@class='lineList']/text()").extract()
+       item["room_traffic"] = response.xpath("//ul[@class='detail_room']/li[5]/span[@class='lineList']//text()").extract()
 
        item["room_desc"] = response.xpath("//div[@class='aboutRoom gray-6']/p/text()").extract_first().strip()
        item["room_allocation"]  = response.xpath("//div[@id='configBox']/ul/li/text()").extract()
 
-       yield item
+       # 获取room_id和house_id
+       room_id = re.findall(r"'roomid', (\d+)", response.body.decode())[0]
+       house_id = re.findall(r"houseid', (\d+)", response.body.decode())[0]
+
+       yield scrapy.Request(
+           url="http://sh.ziroom.com/detail/info?id={}&house_id={}".format(room_id, house_id),
+           callback=self.parse_price,
+           meta={"item": deepcopy(item)}
+       )
+
+    def parse_price(self, response):
+        """ 获取房屋的租金 押金 服务费"""
+        item = response.meta.get("item")
+        json_data = json.loads(response.body.decode())
+        payment = json_data["data"]["payment"]
+        # 月付
+        deposit_pic = payment[0]["deposit"][0]
+        deposit_index = payment[0]["deposit"][2]
+
